4d83fdca68fd1ccf90d89f570059a902cc77d734
matsukawatoshiya <matsukawa5955@gmail.com>
2024-01-26 21:06:24 +0200

    delete serialize from Iembededd

--- sdk/symbol_dart_sdk/bin/models/IEmbeddedTransaction.dart
+++ sdk/symbol_dart_sdk/bin/models/IEmbeddedTransaction.dart
@@ -1,9 +1,6 @@
 import 'dart:typed_data';
 
 interface class IEmbeddedTransaction {
-  dynamic serialize(){
-    throw UnimplementedError('unimplement serialize');
-  }
   dynamic deserialize(Uint8List payload){
     throw UnimplementedError('unimplement deserialize');
   }
