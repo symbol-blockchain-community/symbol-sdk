b89aae705be0d701cc7288f5527f89e80425a1b8
matsukawatoshiya <matsukawa5955@gmail.com>
2024-06-29 08:32:55 +0300

    generate namespaceId

--- sdk/php/src/symbol/idGenerator.php
+++ sdk/php/src/symbol/idGenerator.php
@@ -2,18 +2,87 @@
 
 namespace SymbolSdk\Symbol;
 
+define('NAMESPACE_FLAG', gmp_init('8000000000000000', 16));
+use Exception;
+
 class IdGenerator {
-  public static function generateMosaicId($ownerAddress, $nonce)
-  {
+  public static function generateMosaicId($ownerAddress, $nonce) {
     $hasher = hash_init('sha3-256');
-    hash_update($hasher, pack('V', $nonce));
+    hash_update($hasher, self::uint32ToBytes($nonce));
     hash_update($hasher, $ownerAddress->binaryData);
     $digest = hash_final($hasher, true);
-    $result = unpack('P', $digest)[1];
-  
-    if ($result & 1 << 63)
-      $result -= 1 << 63;
-  
+    $result = self::digestToGmp($digest);
+    
+    $gmpMax = gmp_init('9223372036854775807');
+    
+    if (gmp_testbit($result, 63)) {
+        $result = gmp_sub($result, gmp_add($gmpMax, 1));
+    }
+    
+    return gmp_strval($result);
+  }
+
+  public static function generateNamespaceId($name, $parentNamespaceId = 0) {
+    $hasher = hash_init('sha3-256');
+    hash_update($hasher, self::uint32ToBytes($parentNamespaceId & 0xFFFFFFFF));
+    hash_update($hasher, self::uint32ToBytes(($parentNamespaceId >> 32) & 0xFFFFFFFF));
+    hash_update($hasher, $name);
+    $digest = hash_final($hasher, true);
+    $result = self::digestToGmp($digest);
+    
+    return gmp_strval(gmp_or($result, NAMESPACE_FLAG));
+  }
+
+  public static function isValidNamespaceName($name) {
+    $isAlphanum = function($char) {
+        return ctype_alnum($char);
+    };
+
+    if (!$name || !$isAlphanum($name[0])) {
+        return false;
+    }
+
+    for ($i = 0; $i < strlen($name); ++$i) {
+        $ch = $name[$i];
+        if (!$isAlphanum($ch) && $ch !== '_' && $ch !== '-') {
+            return false;
+        }
+    }
+
+    return true;
+  }
+
+  public static function generateNamespacePath($fullyQualifiedName) {
+    $path = [];
+    $parentNamespaceId = 0;
+    
+    foreach (explode('.', $fullyQualifiedName) as $name) {
+      if (!self::isValidNamespaceName($name)) {
+          throw new Exception("fully qualified name is invalid due to invalid part name ({$fullyQualifiedName})");
+      }
+      
+      $path[] = self::generateNamespaceId($name, $parentNamespaceId);
+      $parentNamespaceId = end($path);
+    }
+    
+    return $path;
+  }
+
+  public static function generateMosaicAliasId($fullyQualifiedName) {
+    $path = self::generateNamespacePath($fullyQualifiedName);
+    return end($path);
+  }
+
+  private static function uint32ToBytes($num) {
+    return pack('V', $num);
+  }
+
+  private static function digestToGmp($digest) {
+    $result = gmp_init(0);
+    for ($i = 0; $i < 8; ++$i) {
+        $shifted = gmp_mul(ord($digest[$i]), gmp_pow(2, 8 * $i));
+        $result = gmp_add($result, $shifted);
+    }
     return $result;
   }
 }
