767de0970efb166d026ff18897a8c6aa42535608
matsukawatoshiya <matsukawa5955@gmail.com>
2024-07-18 20:25:21 +0300

    php版 修正

--- sdk/php/symbol-sdk/tests/symbol/crypto/MosaicIdTest.php
+++ sdk/php/symbol-sdk/tests/symbol/crypto/MosaicIdTest.php
@@ -2,26 +2,23 @@
 
 namespace SymbolSdk\Symbol\Models;
 
-require_once __DIR__ . '/../../../src/symbol/models.php';
-require_once __DIR__ . '/../../../src/symbol/idGenerator.php';
-
 use SymbolSdk\Symbol\IdGenerator;
 use SymbolSdk\Symbol\Address;
 use PHPUnit\Framework\TestCase;
+use SymbolSdk\Utils\Converter;
 
 class MosaicIdTest extends TestCase
 {
   static function idTohex($id)
   {
-    $mosaicId = new UnresolvedMosaicId($id);
-    $hex = strtoupper(gmp_strval(gmp_init($mosaicId->value, 10), 16));
-    $paddedHex = str_pad($hex, 16, '0', STR_PAD_LEFT);
-    return $paddedHex;
+    $mosaicId = new UnresolvedMosaicId($id['id']);
+    $hex = substr($mosaicId->__toString(), 2);
+    return $hex;
   }
 
   public function testGenerateMosaicId()
   {
-    $jsonFilePath = dirname(__DIR__, 5) . '/tests/vectors/symbol/crypto/5.test-mosaic-id.json';
+    $jsonFilePath = dirname(__DIR__, 6) . '/tests/vectors/symbol/crypto/5.test-mosaic-id.json';
     $jsonData = file_get_contents($jsonFilePath);
     $decodedData = json_decode($jsonData, true);
 
